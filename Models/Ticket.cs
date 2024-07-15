57ad772c34c2d3b18b0724321a991610e23746bd
jmhamilton2022 <jmhamilton2022@outlook.com>
2024-07-15 16:16:02 -0500

    Added validation to Ticket Model updated controller and views

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -8,15 +8,19 @@ namespace ToDoList.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Please enter a name.")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
         public string Name { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Please enter a description.")]
+        [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters.")]
         public string Description { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Please enter a sprint number.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Sprint number must be greater than 0.")]
         public int SprintNumber { get; set; }
 
         [Required(ErrorMessage = "Please enter a point value.")]
+        [Range(1, 100, ErrorMessage = "Point value must be between 1 and 100.")]
         public int PointValue { get; set; }
 
         [Required(ErrorMessage = "Please select a status.")]
